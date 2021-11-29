3c3f812f3f29cc4f20cdb927ab84ef563fa6d4bc
Rafael Gonzalez <rafygonzalez089@gmail.com>
2021-11-28 20:05:35 -0500

    [Add] Add toJson Receiver Func

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 func main(){
 	server := NewServer(":3000")
 	server.Handle("GET","/", server.AddMiddleware(HandleRoot, CheckAuth(), Logging()))
+	server.Handle("POST","/create", PostRequest)
 	server.Handle("POST","/auth", HandleAuth)
+	server.Handle("POST","/auth/signup", UserPostRequest)
 	server.Listen()
 }
